cdb372287d571cd7f26e711f4fce645fcd419c5e
yangemba <yangemba@gmail.com>
2019-02-06 01:52:20 +0200

    done

--- HelperTestBase.py
+++ HelperTestBase.py
@@ -2,9 +2,8 @@ import unittest
 
 class HelperTestBase(unittest.TestCase):
 
-    def __init__(self):
+    def __init__(self): # заменил на конструктор иначе плохо импортирует
         self.base_url = 'https://premier.ua/zhilaia-nedvizhimost'
-
 
 add_point_list = ['/arenda-zhilia', '/pokupka-zhilia', '/kvartiry-v-novostroykakh', '/prodazha-mn-kh-elitnykh',
                   '/garazhi']
--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import requests
 from HelperTestBase import HelperTestBase, add_point_list
 from urllib.parse import urljoin
 
+'''urljoin  - хороший метод для слияния основного УРЛа с Эдд-поинт. Так как даже если ты осташь "/" и в коне УКРла и в 
+начале Эдд-поинта - то он уберет лишний. Экономит кучу времени на поиск ошибки'''
 
-add_point_qnt = len(add_point_list)
+add_point_qnt = len(add_point_list) # Кол-во елеметов в эдд поинте (фактическое, начинаеться с 1-го)
 print(add_point_qnt)
 
 class TestCheckStatus(HelperTestBase):
 
-    counter = 0
+    counter = 0 # - счетчик для проверки что мы не вышли закол-во. По дэфолту ноль, так как первый элемент - нулевой
     def test_CheckStatusCode(self,):
 
+        for element in add_point_list: #Проходка по каждому элементу
 
-        for element in add_point_list:
-
-            if self.counter != add_point_qnt - 1:
-                url = urljoin(self.base_url, add_point_list[self.counter])
+            if self.counter != add_point_qnt - 1:# "-1" отнимает от фактического, так как индекс должен быть пайтоновский
+                url = urljoin(self.base_url, add_point_list[self.counter])# собственно сам индекс
                 response = requests.get(url)
                 # print(response)
                 if response.status_code == 200:
